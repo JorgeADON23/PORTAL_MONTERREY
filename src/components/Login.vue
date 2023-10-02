8e91f21e16e237fb78b19d9e508d01f64552c2f3
JorgeADON23 <adon2023@outloo.es>
2023-10-02 12:20:56 -0600

    Primer commit

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <p>Login</p>
+        <input type="text" class="form-control">
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name: "ComponenteLogin"
+}
+
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+    <Login></Login>
+</template>
+
+
+
+<script>
+
+import Login from "../components/Login.vue"
+
+
+export default {
+    name: "LoginView",
+    components: {
+        Login
+    }
+}
+
+</script>
